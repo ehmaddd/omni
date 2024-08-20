3a345605e1b19ca208a20046c522070af7cd6fa3
ehmaddd <ehmaddd@gmail.com>
2024-08-20 13:09:20 +0500

    Fix closing bracket issue

--- src/server.js
+++ src/server.js
@@ -29,7 +29,7 @@ app.post('/register', async (req, res) => {
 
 app.get('/dashboard/:userId', authenticateToken, (req, res) => {
   const { userId } = req.params;
-}
+});
 
 // Login route
 app.post('/login', async (req, res) => {
