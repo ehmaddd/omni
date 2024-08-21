c17ea8162c42aea10fdcab1543716aa948dc5869
ehmaddd <ehmaddd@gmail.com>
2024-08-21 16:23:25 +0500

    Adjust provider issue

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';  // Import BrowserRouter
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../src/components/redux/store';
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
