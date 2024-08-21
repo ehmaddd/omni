0ce78474dd18bb5fdc62d8fca3448596b745f5fa
ehmaddd <ehmaddd@gmail.com>
2024-08-21 10:15:10 +0500

    Add history reducer in store

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import weatherReducer from './weatherSlice';
-import prayerReducer from './prayerSlice';  // Import prayerReducer
+import prayerReducer from './prayerSlice';
+import historyReducer from './features/historySlice';
 
 const store = configureStore({
   reducer: {
     weather: weatherReducer,
-    prayer: prayerReducer
+    prayer: prayerReducer,
+    history: historyReducer
   }
 });
 
