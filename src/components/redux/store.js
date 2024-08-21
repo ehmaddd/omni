7e8e00a6e27e76e60931726cd7d1bde3bfa41aa2
ehmaddd <ehmaddd@gmail.com>
2024-08-21 10:20:24 +0500

    Remove unused files

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import weatherReducer from './weatherSlice';
 import prayerReducer from './prayerSlice';
-import historyReducer from './features/historySlice';
 
 const store = configureStore({
   reducer: {
     weather: weatherReducer,
     prayer: prayerReducer,
-    history: historyReducer
   }
 });
 
